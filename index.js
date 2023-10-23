1eba1cc775df93399b05ac47c10d11395456a4ab
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-10-23 22:36:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/callback', async (req, res) => {
         console.log('Response text:', req.body.message.text);
         
         res.setHeader('Content-Type', 'application/json');
-        res.json({ messagePayload: JSON.stringify(req.body.messagePayload)});
+        res.json({ messagePayload: req.body.message.text});
     } catch (error) {
         console.error('Error calling the API:', error.response ? error.response.data : error.message);
         res.status(500).json({ status: 'error', message: 'Failed to call the API' });
