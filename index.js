0e6709e467d0948fae9adeb3132a0614e91514b8
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-10-25 17:26:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ const handleRequest2 = async (req, res) => {
         });
         console.log('please', req.body);
         // If the incoming request has messagePayload, then return it
-        if (req.body.payload.payload) {
+        // if (req.body.payload.payload) {
             const chatbotReply = await axios.post('https://whatsapp-wo7o.onrender.com/chatbot-reply', { message: response.data.message });
             console.log('give', chatbotReply);
         // Return the message from the chatbot-reply response
             return res.json({ messagePayload: chatbotReply.data.messagePayload });
-        } else {
-            console.log('Response Data from API2:', response.data);
-            console.log('Received request:', req.body);
-            // return res.json(req.body);
-        }
+        // } else {
+        //     console.log('Response Data from API2:', response.data);
+        //     console.log('Received request:', req.body);
+        //     // return res.json(req.body);
+        // }
 
     } catch (error) {
         console.error('Error calling the API 3:', error.response ? error.response.data : error.message);
@@ -56,13 +56,15 @@ app.post('/callback', async (req, res) => {
 
 
 app.post('/chatbot-reply', async (req, res) => {
-    console.log('Received reply from chatbot:', req.body.message);
-    
-
-    await handleRequest2(req, res);
-    return res.json({ messagePayload: req.body.message });
+    try {
+        console.log('Received reply from chatbot:', req.body);
+        const result = await handleRequest2(req.body);
+        res.json(result);
+    } catch (error) {
+        console.error('Error processing chatbot reply:', error);
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
