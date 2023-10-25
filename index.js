dd9ab4cd9867ee524ed8913894450966795887c4
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-10-25 14:02:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -233,9 +233,10 @@ app.post('/callback', async (req, res) => {
 
 app.post('/chatbot-reply', async (req, res) => {
     console.log('Received reply from chatbot:', req.body.message);
-    return res.json({ messagePayload: req.body.message });
+    
 
-    // await handleRequest2(req, res);
+    await handleRequest2(req, res);
+    return res.json({ messagePayload: req.body.message });
 });
 
 const PORT = process.env.PORT || 3000;
