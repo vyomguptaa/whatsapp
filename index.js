5f450519a72be63c59022db297ddabd5182713aa
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-10-25 14:34:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -210,15 +210,19 @@ const handleRequest2 = async (req, res) => {
         });
         console.log('please', req.body);
         // If the incoming request has messagePayload, then return it
-        if (req.body.messagePayload) {
-            console.log('Response Data from API1:', req.body);
-            console.log('Received reply from chatbot handle:', req.body.message);
-            return res.json({ messagePayload: req.body.messagePayload });
-        } else {
-            console.log('Response Data from API2:', response.data);
-            console.log('Received request:', req.body);
-            // return res.json(req.body);
-        }
+        // if (req.body.messagePayload) {
+        //     console.log('Response Data from API1:', req.body);
+        //     console.log('Received reply from chatbot handle:', req.body.message);
+        //     return res.json({ messagePayload: req.body.messagePayload });
+        // } else {
+        //     console.log('Response Data from API2:', response.data);
+        //     console.log('Received request:', req.body);
+        //     // return res.json(req.body);
+        // }
+        const chatbotReply = await axios.post('https://whatsapp-wo7o.onrender.com/chatbot-reply', { message: response.data.message });
+
+        // Return the message from the chatbot-reply response
+        return res.json({ messagePayload: chatbotReply.data.messagePayload });
     } catch (error) {
         console.error('Error calling the API 3:', error.response ? error.response.data : error.message);
         res.status(500).json({ status: 'error', message: 'Failed to call the API' });
