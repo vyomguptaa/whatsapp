908b23a0543a2e583911d9f582061fab1cc01d60
Vyom Gupta <vyom.gupta2020@vitstudent.ac.in>
2023-11-07 14:49:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,4 @@
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const axios = require('axios');
-// const { EventEmitter } = require('events');
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// const API_URL = 'https://conv.chatclay.com/webhook/voice';
-// const API_KEY = 'JXNHGFmWMDFWZ4LtdHYStE'; 
-
-// const events = new EventEmitter();  // Create an event emitter
-
-// app.post('/chatbot-reply', async (req, res) => {
-//     console.log('Received reply from chatbot 2:', req.body);
-//     events.emit('receivedChatbotReply', req.body);  // Emit event when data is received
-//     return res.json({ messagePayload: req.body });
-// });
-
-// const handleRequest2 = async (req, res) => {
-//     // console.log(req.body);
-//     if (!req.body.payload || !req.body.payload.payload || !req.body.payload.payload.text) {
-//         console.log('The text property is not defined, returning nothing.');
-//         return res.status(400).json({ status: 'error', message: 'No text provided' });
-//     }
-//     const dataToSend = {
-//         bot: "6541f452234d442ffb2dc202",
-//         sender: {
-//             id: "6505d8ffbd59247f06e0ebaa",
-//             // id: req.body.payload.id,
-//             name: req.body.payload.sender.name,
-//             data: {}
-//         },
-//         message: {
-//             text: req.body.payload.payload.text,
-//             locale: ""
-//         },
-//         timestamp: req.body.timestamp
-//     };
-
-//     try {
-//         const response = await axios.post(API_URL, dataToSend, {
-//             headers: {
-//                 'x-api-key': API_KEY,
-//                 'content-type': 'application/json'
-//             }
-//         });
-
-//         // console.log('please', response.data);
-
-//         // Await for the event to be emitted .......
-//         const answer = await new Promise(resolve => events.once('receivedChatbotReply', resolve));
-//         let parsedPayload = JSON.parse(answer.messagePayload);
-//         console.log('Received text:', parsedPayload.text);
-//         return res.send(parsedPayload.text);
-
-//     } catch (error) {
-//         console.error('Error calling the API 3:', error.response ? error.response.data : error.message);
-//         res.status(500).json({ status: 'error', message: 'Failed to call the API' });
-//     }
-// };
-
-// app.post('/callback', async (req, res) => {
-//     // console.log('Received request from Gupshup:', req.body);
-//     await handleRequest2(req, res);
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server started on port ${PORT}`);
-// });
-//
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
@@ -175,8 +103,8 @@ const allMessages = await collectMessages();
               sender: {
                 phone: req.body.payload.sender.phone,
                 name: req.body.payload.sender.name,
-                country_code: req.body.payload.sender.country_code, // Replace with actual country code if available
-                dial_code: req.body.payload.sender.dial_code, // Replace with actual dial code if available
+                country_code: req.body.payload.sender.country_code,
+                dial_code: req.body.payload.sender.dial_code,
               },
             }
           };
@@ -191,31 +119,12 @@ const allMessages = await collectMessages();
     // If there are quick replies, send them, otherwise send the combined text
     if (quickRepliesFormatted.length > 0) {
       // return res.json({ messages: combinedMessageText.trim(), quickReplies: quickRepliesFormatted });
-      return res.send(quickRepliesFormatted);
+      // return res.send(quickRepliesFormatted);
+      return res.send(combinedMessageText.trim());
     } else {
       return res.send(combinedMessageText.trim());
     }
 
-    // Wait for all messages to be received
-    // const allMessages = await collectMessages();
-    // let combinedMessageText = '';
-    // allMessages.forEach(m => {
-    //   const payload = JSON.parse(m.messagePayload);
-    //   combinedMessageText += payload.text + '\n';
-    //   if (payload.quick_replies) {
-    //     console.log('Quick replies:', payload.quick_replies);
-    //   }
-    // });
-    // return res.send(combinedMessageText.trim());
-    // new
-    
-    // const payload3 = JSON.parse(m.messagePayload);
-    // if (payload3.quick_replies) {
-    //     console.log('Quick replies:', payload.quick_replies);
-    //   }
-    // const combinedMessageText = allMessages.map(m => JSON.parse(m.messagePayload).text).join('\n');
-
-    // return res.send(combinedMessageText);
 
   } catch (error) {
     console.error('Error calling the chatbot API:', error.response ? error.response.data : error.message);
